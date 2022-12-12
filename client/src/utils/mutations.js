96104375b639ac113247e0713ad4704d95d9c41d
oliverLo78 <oliverberto@gmail.com>
2022-12-12 15:38:09 -0500

    cannot find module utils/auth

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,10 +1,27 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation loginUser() {
-    me {
-      _id
-      name
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
   }
 `;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// export const SAVE_BOOK = gql`
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -7,8 +7,8 @@ const expiration = '2h';
 module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    // allows token to be sent via  req.query, req.body or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
