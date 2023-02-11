c6058de95ea0a741f633330b61a1026eec31c699
oliverLo78 <oliverberto@gmail.com>
2023-02-11 11:19:40 -0500

    changed index.js  to server.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,13 +1,20 @@
-import React, { useState, useEffect } from "react";
-import { Col, Row, Container, Card, Button } from "react-bootstrap";
+import { useQuery, useMutation } from '@apollo/client';
 
+import { REMOVE_BOOK } from '../utils/mutations';
+import React, { useState, useEffect } from "react";
+import { 
+  Col,
+  Row,
+  Container,
+  Card, 
+  Button } from "react-bootstrap";
 import { getMe, deleteBook } from "../utils/API";
 import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
-
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
 
